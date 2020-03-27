d174db667e9bffb5c2fd3c831a79d24418b8b916
Mika Katua <44744529+mikakatua@users.noreply.github.com>
2020-03-27 12:34:05 +0100

    Update aws_logs.sh

--- aws_logs.sh
+++ aws_logs.sh
@@ -32,7 +32,7 @@ do
   then
     line=$(echo $LOG_STREAM $FIRST_EVENT $LAST_EVENT | awk '{$2=strftime("%Y-%m-%d %H:%M:%S", $2/1000);$3=strftime("%Y-%m-%d %H:%M:%S", $3/1000); print $0}')
     echo -e "\n\033[1m$line:\033[0m"
-    aws logs get-log-events --log-group-name $LOG_GROUP --log-stream-name $LOG_STREAM --query "events[*].[timestamp,message]" $GETLOGEVENTS_ARGS | awk '{$1=strftime("%Y-%m-%d %H:%M:%S", $1/1000); print $0}'
+    aws logs get-log-events --log-group-name $LOG_GROUP --log-stream-name $LOG_STREAM --query "events[*].[timestamp,message]" $GETLOGEVENTS_ARGS --output text | sed '/^$/d' | awk '{$1=strftime("%Y-%m-%d %H:%M:%S", $1/1000); print $0}'
   fi
-done < <(aws logs describe-log-streams --log-group-name $LOG_GROUP --query "logStreams[*].[logStreamName,firstEventTimestamp,lastEventTimestamp]" | sort -n -k 2)
+done < <(aws logs describe-log-streams --log-group-name $LOG_GROUP --query "logStreams[*].[logStreamName,firstEventTimestamp,lastEventTimestamp]" --output text | sort -n -k 2)
 
